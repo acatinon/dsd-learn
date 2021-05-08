6ff85727b43318fae10aacc215f798c507031b4b
Aurel <aurelien.catinon@gmail.com>
2021-05-08 10:04:23 +0200

    Adding "Epochs, TWAP, expansions and contractions"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   theme: {
+    colors: {
+      primary:  {
+        light: '#3981e8',
+        DEFAULT: '#3981e8',
+        dark: '#3981e8'
+      },
+      secondary: {
+        light: '#66dbd8',
+        DEFAULT: '#66dbd8',
+        dark: '#66dbd8'
+      },
+    },
     container: {
       center: true,
       padding: '20rem',
